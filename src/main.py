b124d1bd9b264e8126eeffb6df14cc604385203c
kamicup <yoshikazu.hashimoto@gmail.com>
2023-04-30 18:15:31 +0900

    クリエイターの stats を追加してみる

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
-import requests
 import csv
-import time
 import datetime
+import time
+
+import requests
 
 
-def load(page=1):
-    url = 'https://api.cluster.mu/v1/worlds/search/func:LatestWorld?page={}'.format(page)
-
+def get_json(url):
     headers = {
         'authority': 'api.cluster.mu',
         'accept': 'application/json, text/plain, */*',
@@ -26,70 +25,120 @@ def load(page=1):
         'x-cluster-platform': 'Web',
         'accept-encoding': 'gzip, deflate, br'
     }
-
     response = requests.get(url, headers=headers)
-
-    # レスポンスの内容を取得する
     content = response.json()
-
     return content
 
 
-def flatten(worlds):
-    items = []
-    for item in worlds:
-        items.append([
-            item['webUrl'],
-            "{:.2f}".format(item['likeCount'] / item['playCount']) if item['playCount'] else 0,
-            item['likeCount'],
-            item['playCount'],
-            item['name'],
-            item['privacyType'],
-            item['allowEvent'],
-            item['category'],
-            item['sdkType'],
-            item['venueSize'],
-            item['venueCreatedAt'],
-            item['venuePublishedAt'],
-            item['venueUpdatedAt'],
-            # item['description'],
-            item['creator']['shareUrl'],
-            item['creator']['displayName'],
-            item['creator']['username'],
-            item['creator']['isBeginner'],
-            item['creator']['isCertified'],
-            item['creator']['isDeleted'],
-        ])
-    return items
+def search_latest_world(page=1):
+    url = 'https://api.cluster.mu/v1/worlds/search/func:LatestWorld?page={}'.format(page)
+    return get_json(url)
+
+
+def users_profile(username):
+    url = 'https://api.cluster.mu/v1/users/profile/{}'.format(username)
+    return get_json(url)
+
+
+def events_created_by(user_id):
+    url = 'https://api.cluster.mu/v1/events?createdBy={}'.format(user_id)
+    return get_json(url)
+
+
+def user_friends(user_id):
+    url = 'https://api.cluster.mu/v1/user_friends/{}'.format(user_id)
+    return get_json(url)
+
+
+def worlds_created_by(user_id):
+    url = 'https://api.cluster.mu/v1/worlds?createdBy={}'.format(user_id)
+    return get_json(url)
+
+
+class ClusterWorldSearch:
+    def __init__(self):
+        self.profile_cache = {}
+
+    def get_profile(self, user_id):
+        if user_id in self.profile_cache:
+            return self.profile_cache[user_id]
+
+        time.sleep(1)
+        if user_id:
+            events = events_created_by(user_id)['events']
+            friends = user_friends(user_id)['users']
+            worlds = worlds_created_by(user_id)['worlds']
+            num_events = len(events)
+            num_friends = len(friends)
+            num_worlds = len(worlds)
+        else:
+            num_worlds = num_events = num_friends = None
+
+        value = (num_events, num_friends, num_worlds)
+        self.profile_cache[user_id] = value
+        return value
+
+    def save_tsv(self, filename):
+        with open(filename, 'w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter='\t')
+
+            max_num_worlds = 50000
+            items_per_page = 24
+            max_page = int(max_num_worlds / items_per_page)
+            for page in range(1, max_page):
+                content = search_latest_world(page)
+
+                page_data = content['pageData']
+                worlds = content['worlds']
+
+                if len(worlds) == 0:
+                    break
+
+                for item in worlds:
+                    (num_events, num_friends, num_worlds) = self.get_profile(item['creator']['userId'])
+
+                    row = [
+                        item['webUrl'],
+                        "{:.2f}".format(item['likeCount'] / item['playCount']) if item['playCount'] else 0,
+                        item['likeCount'],
+                        item['playCount'],
+                        item['name'],
+                        item['privacyType'],
+                        item['allowEvent'],
+                        item['category'],
+                        item['sdkType'],
+                        item['venueSize'],
+                        item['venueCreatedAt'],
+                        item['venuePublishedAt'],
+                        item['venueUpdatedAt'],
+                        # item['description'],
+                        item['creator']['shareUrl'],
+                        item['creator']['displayName'],
+                        item['creator']['username'],
+                        item['creator']['isBeginner'],
+                        item['creator']['isCertified'],
+                        item['creator']['isDeleted'],
+                        num_worlds,
+                        num_events,
+                        num_friends,
+                    ]
+                    csv_writer.writerow(row)
+                    print(row)
+                    csv_file.flush()
+
+                print(page_data)
+                time.sleep(1)
 
 
 # メイン処理
 def main():
-    # TSV形式で出力する
-
     formatted_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = "{}.tsv".format(formatted_time)
 
-    with open(filename, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter='\t')
-
-        expected_total = 500000
-        items_per_page = 24
-        for page in range(1, int(expected_total/items_per_page)):
-            content = load(page)
-
-            page_data = content['pageData']
-            worlds = content['worlds']
-
-            # print(json.dumps(worlds, indent=4))
-
-            items = flatten(worlds)
-            writer.writerows(items)
-
-            print(page_data)
-            time.sleep(2)
+    ClusterWorldSearch().save_tsv(filename)
 
 
 # メイン処理の実行
 if __name__ == '__main__':
     main()
+
